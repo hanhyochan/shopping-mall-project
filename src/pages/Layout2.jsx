c8d438eb96331ca252f86c7b8aef9c16926b8971
hanhyochan <hcartist569@gmail.com>
2024-11-18 22:23:21 +0900

    test

---
+++ src/pages/Layout2.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import Header from '../components/common/Header';
+// import Footer from '../components/common/Footer';
+import { Layout } from "antd";
+
+const Layout2 = () => {
+
+    return (
+        <>
+            <Layout>
+                dfsd
+            </Layout>
+        </>
+    );
+};
+
+export default Layout2;
