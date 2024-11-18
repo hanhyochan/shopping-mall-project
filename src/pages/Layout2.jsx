1871d373b64247537f90efaa06a6131b8e623535
hanhyochan <hcartist569@gmail.com>
2024-11-18 23:13:34 +0900

    상단바 템플릿 시험 적용

--- src/pages/Layout2.jsx
+++ src/pages/Layout2.jsx
@@ -1,17 +1,109 @@
-import React from 'react';
-// import Header from '../components/common/Header';
-// import Footer from '../components/common/Footer';
-import { Layout } from "antd";
-
+import React, { useState } from 'react';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+const items = [
+    {
+        label: '운동복',
+        key: 'SubMenu1',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: 'Option 1',
+                key: 'setting:1',
+            },
+            {
+                label: 'Option 2',
+                key: 'setting:2',
+            },
+        ]
+    },
+    {
+        label: '운동복',
+        key: 'SubMenu2',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: 'Option 1',
+                key: 'setting:1',
+            },
+            {
+                label: 'Option 2',
+                key: 'setting:2',
+            },
+        ]
+    },
+    {
+        label: '운동복',
+        key: 'SubMenu3',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: 'Option 1',
+                key: 'setting:1',
+            },
+            {
+                label: 'Option 2',
+                key: 'setting:2',
+            },
+        ]
+    },
+    {
+        label: '운동복',
+        key: 'SubMenu4',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: 'Option 1',
+                key: 'setting:1',
+            },
+            {
+                label: 'Option 2',
+                key: 'setting:2',
+            },
+        ]
+    },
+    {
+        label: '운동복',
+        key: 'SubMenu5',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: 'Option 1',
+                key: 'setting:1',
+            },
+            {
+                label: 'Option 2',
+                key: 'setting:2',
+            },
+        ]
+    },
+    {
+        label: '운동복',
+        key: 'SubMenu6',
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: 'Option 1',
+                key: 'setting:1',
+            },
+            {
+                label: 'Option 2',
+                key: 'setting:2',
+            },
+        ]
+    }
+];
 const Layout2 = () => {
-
+    const [current, setCurrent] = useState('mail');
+    const onClick = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
     return (
         <>
-            <Layout>
-                dfsd
-            </Layout>
+        하이
+        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
         </>
-    );
+    )
 };
-
 export default Layout2;
