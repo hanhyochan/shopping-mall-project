3d9c01e53e4684f9365e93f34a2a90c294e46751
yoonhyeonjeong <h_j111@naver.com>
2024-11-18 23:23:53 +0900

    antd test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Layout2 from './pages/Layout2';
+import React from "react";
+import Layout2 from "./pages/Layout2";
+import Layout3 from "./pages/Layout3";
 
 const App = () => {
-  return (
-    <div>
-      <Layout2 />
-    </div>
-  );
+    return (
+        <div>
+            <Layout2 />
+            <Layout3 />
+        </div>
+    );
 };
 
 export default App;
---
+++ src/pages/Layout3.jsx
@@ -0,0 +1,124 @@
+import React, {useState} from "react";
+import {AppstoreOutlined, MailOutlined, SettingOutlined} from "@ant-design/icons";
+import {Menu} from "antd";
+import {Typography} from "antd";
+
+const {Title} = Typography;
+const items = [
+    {
+        label: "운동복",
+        key: "SubMenu1",
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: "Option 1",
+                key: "setting:1",
+            },
+            {
+                label: "Option 2",
+                key: "setting:2",
+            },
+        ],
+    },
+    {
+        label: "운동복",
+        key: "SubMenu2",
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: "Option 1",
+                key: "setting:1",
+            },
+            {
+                label: "Option 2",
+                key: "setting:2",
+            },
+        ],
+    },
+    {
+        label: "운동복",
+        key: "SubMenu3",
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: "Option 1",
+                key: "setting:1",
+            },
+            {
+                label: "Option 2",
+                key: "setting:2",
+            },
+        ],
+    },
+    {
+        label: "운동복",
+        key: "SubMenu4",
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: "Option 1",
+                key: "setting:1",
+            },
+            {
+                label: "Option 2",
+                key: "setting:2",
+            },
+        ],
+    },
+    {
+        label: "운동복",
+        key: "SubMenu5",
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: "Option 1",
+                key: "setting:1",
+            },
+            {
+                label: "Option 2",
+                key: "setting:2",
+            },
+        ],
+    },
+    {
+        label: "운동복",
+        key: "SubMenu6",
+        icon: <SettingOutlined />,
+        children: [
+            {
+                label: "Option 1",
+                key: "setting:1",
+            },
+            {
+                label: "Option 2",
+                key: "setting:2",
+            },
+        ],
+    },
+];
+const Layout3 = () => {
+    const [current, setCurrent] = useState("mail");
+    const onClick = e => {
+        console.log("click ", e);
+        setCurrent(e.key);
+    };
+    return (
+        <>
+            <div className="flex justify-between">
+                <Title
+                    level={3}
+                    style={{margin: 0}}
+                >
+                    MyApp
+                </Title>
+                <Menu
+                    onClick={onClick}
+                    selectedKeys={[current]}
+                    mode="horizontal"
+                    items={items}
+                />
+            </div>
+        </>
+    );
+};
+export default Layout3;
