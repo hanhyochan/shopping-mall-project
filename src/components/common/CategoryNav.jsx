dcbdf7b6abb5de5ffa65807e22f14b30f85bc78f
hanhyochan <hcartist569@gmail.com>
2024-11-21 15:25:06 +0900

    first commit

--- src/components/common/CategoryNav.jsx
+++ src/components/common/CategoryNav.jsx
@@ -76,7 +76,7 @@ const items = [
 
 const CategoryNav = () => {
     return (
-        <div className="flex items-center justify-between gap-4 relative">
+        <div className="relative flex items-center justify-between gap-4">
             <div>
                 <Button
                     className="border-0"
@@ -85,7 +85,7 @@ const CategoryNav = () => {
                 <Button className="underline border-0">Best 10</Button>
             </div>
 
-            <div className="flex-1 flex justify-center absolute left-1/2 transform -translate-x-1/2">
+            <div className="absolute flex justify-center flex-1 transform -translate-x-1/2 left-1/2">
                 <Menu
                     mode="horizontal"
                     items={items}
@@ -93,9 +93,9 @@ const CategoryNav = () => {
                 />
             </div>
 
-            <div className="flex items-center gap-2 w-64">
+            <div className="flex items-center w-64 gap-2">
                 <Input
-                    placeholder="상품을 검색해보세요"
+                    placeholder="상품을 검색해보세요.."
                     className="w-full"
                 />
                 <Button
