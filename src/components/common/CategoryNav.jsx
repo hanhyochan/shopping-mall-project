3760123a00de1349f33d54ccc78aaf1c22de6fad
yoonhyeonjeong <h_j111@naver.com>
2024-11-22 17:40:48 +0900

    modify CategoryNav 반응형수정

--- src/components/common/CategoryNav.jsx
+++ src/components/common/CategoryNav.jsx
@@ -1,6 +1,5 @@
 import {SearchOutlined, MenuOutlined} from "@ant-design/icons";
 import {Menu, Input, Button} from "antd";
-
 const items = [
     {
         label: "의류",
@@ -76,27 +75,27 @@ const items = [
 
 const CategoryNav = () => {
     return (
-        <div className="flex items-center justify-between gap-4 relative md:w-[80vw] xl: w-[90vw]">
-            <div>
+        <div className="flex items-center flex-col sm:flex-row justify-between gap-4 relative md:w-[80vw] xl: w-[90vw]">
+            <div className="hidden md:block">
                 <Button
                     className="border-0"
                     icon={<MenuOutlined />}
                 />
-                <Button className="underline border-0">Best 10</Button>
+                <Button className="underline border-0 text-base">Best 10</Button>
             </div>
 
-            <div className="flex-1 flex justify-center absolute left-1/2 transform -translate-x-1/2">
+            <div className="flex-1 flex justify-center static md:absolute md:left-1/2 md:transform md:-translate-x-1/2">
                 <Menu
                     mode="horizontal"
                     items={items}
-                    className="!border-0 min-w-350"
+                    className="!border-0 min-w-350 text-base"
                 />
             </div>
 
-            <div className="flex items-center gap-2 w-250">
+            <div className="flex items-center gap-2 sm:w-250 w-full">
                 <Input
                     placeholder="상품을 검색해보세요"
-                    className="w-full"
+                    className="w-full text-base"
                 />
                 <Button
                     className="border-0"
