87f6e98201a7e056c967d99f1b78186592a33578
yoonhyeonjeong <h_j111@naver.com>
2024-11-26 21:16:53 +0900

    install postcss-pxtorem

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
 export default {
     plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+        "tailwindcss": {},
+        "autoprefixer": {},
+        "postcss-pxtorem": {
+            rootValue: 16, // 기본 값 (보통 16px)
+            unitPrecision: 5,
+            propList: ["*"], // 모든 속성에 적용
+            selectorBlackList: [], // 특정 셀렉터 제외
+            replace: true,
+            mediaQuery: false,
+        },
     },
-  };
-  
+};
