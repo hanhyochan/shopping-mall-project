79dabf0e567fa1141de0b6436561c959921fa260
yoonhyeonjeong <h_j111@naver.com>
2024-11-27 17:38:36 +0900

    Add sticky navbar and restore it on category change

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -2,18 +2,12 @@ import Footer from "./Footer";
 import TopNav from "./TopNav";
 import CategoryNav from "./CategoryNav";
 import React from "react";
-import {useLocation} from "react-router-dom";
 
 const Layout = ({children}) => {
-    const {search} = useLocation(); // url에서 쿼리파라미터추출
-    const query = new URLSearchParams(search);
-    const subcategory = query.get("subcategory");
-
     return (
         <>
             <TopNav />
-            {/* 서브카테고리 경로로 갔을때 카테고리 네비바 숨기기 */}
-            {subcategory ? null : <CategoryNav />}
+            <CategoryNav />
             <main className="md:w-[80vw] xl: w-[90vw] mx-auto pb-[450px]">{children}</main>
             <Footer />
         </>
